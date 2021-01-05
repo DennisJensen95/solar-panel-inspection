b1eb7348b4982a0f564b8135e9cd0a0ea295f1ef
theismunk <s164022@student.dtu.dk>
2021-01-05 18:57:13 +0100

    data loader: fjerner alle paths uden labels og plotter et eksempel billede

--- inspection_software/components/data_loader/data_load.py
+++ inspection_software/components/data_loader/data_load.py
@@ -9,11 +9,18 @@ from fastai.vision.all import *
 
 class solar_panel_data():
 
-    def __init__(self, img_dir, gt_dir):
+    def __init__(self, img_dir, gt_dir, transforms = None, removeNoLabels = True):
         self.ImageDir = img_dir + "*"
         self.GTDir = gt_dir + "*"
 
+        self.transforms = transforms
+
         self.files, self.masks = self.Load()
+
+        if removeNoLabels:
+            print("Removing files without labels from path...")
+            self.RemoveNoLabels()
+        
 
 
     def Load(self):
@@ -60,6 +67,40 @@ class solar_panel_data():
         # Add the path back and return
         return [a[0][:(n_f+19)] + w + a[0][-4:] for w in names]
 
+    def RemoveNoLabels(self):
+        names_m = []
+        for i in range(len(self.masks)):
+            GT = sci.loadmat(self.masks[i])
+            Labelstemp = GT['GTLabel'] # fault labels
+
+            if Labelstemp.size > 0:
+                names_m.append(self.masks[i])
+        
+        self.masks = names_m
+        n_f = len(self.ImageDir)-1
+        n_m = len(self.GTDir)-1
+        self.files = self.RemoveNoMatches(self.files,names_m, n_f, n_m)
+
+    def PrintPaths(self):
+        for i in range(len(self.files)):
+            print(self.files[i])
+            print(self.masks[i])
+            print("------------------------")
+
+    # def get_y(self):
+    #     # Load .mat file
+    #     GT = sci.loadmat(path)
+    #     mask = GT['GTMask'] # fault mask
+    #     return mask
+
+    # def __getDatablock__(self):
+    #     codes = ["No Failure", "Crack A", "Crack B", "Crack C", "Finger Failure"]
+    #     return DataBlock(blocks = (ImageBlock, MaskBlock(codes=codes)),
+    #                      get_items = get_image_files,
+    #                      get_y = self.get_y,
+    #                      splitter  = RandomSplitter(),
+    #                      )
+
     def __getitem__(self, idx):
         # load images and masks
         img_path = self.files[idx]
@@ -73,32 +114,39 @@ class solar_panel_data():
         # Load example image
         img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
-        new_labels = ["No Failure"]
-        for i in range(len(Labels[0])):
-            new_labels.append(Labels[0][i][0])
-        
-        # 0: background
-        # instances are encoded as different colors
-        obj_ids = np.unique(mask)
-        # first id is the background, so remove it
-        # obj_ids = obj_ids[1:]
+        if Labels.size == 0:
+            new_labels = []
+            labels = []
+            boxes = [[]]
+            masks = mask
 
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = mask == obj_ids[:, None, None]
-        masks = masks.astype(np.uint8)
-        
-        # get bounding box coordinates for each mask
-        boxes = []
-        for i in range(len(obj_ids)):
-            cnt = cv2.findContours(masks[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-            for c in cnt:
-                x,y,w,h = cv2.boundingRect(c)
-                xmin = x
-                xmax = x+w
-                ymin = y
-                ymax = y+h
-                boxes.append([xmin, ymin, xmax, ymax])
+        else:
+            new_labels = []
+            for i in range(len(Labels[0])):
+                new_labels.append(Labels[0][i][0])
+            
+            # 0: background
+            # instances are encoded as different colors
+            obj_ids = np.unique(mask)
+            # first id is the background, so remove it
+            obj_ids = obj_ids[1:]
+
+            # split the color-encoded mask into a set
+            # of binary masks
+            masks = mask == obj_ids[:, None, None]
+            masks = masks.astype(np.uint8)
+            
+            # get bounding box coordinates for each mask
+            boxes = []
+            for i in range(len(obj_ids)):
+                cnt = cv2.findContours(masks[i], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+                for c in cnt:
+                    x,y,w,h = cv2.boundingRect(c)
+                    xmin = x
+                    xmax = x+w
+                    ymin = y
+                    ymax = y+h
+                    boxes.append([xmin, ymin, xmax, ymax])
 
         # convert everything into a torch.Tensor
         boxes = torch.as_tensor(boxes, dtype=torch.float32)
@@ -118,11 +166,13 @@ class solar_panel_data():
         target["image_id"] = image_id
         target["area"] = area
         
-        # if self.transforms is not None:
-        #     img, target = self.transforms(img, target)
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
 
         return img, target
 
+    def __len__(self):
+        return len(self.files)
 
     def __getlabel__(self, labels):
         """Extract numerical label from label string
@@ -137,8 +187,7 @@ class solar_panel_data():
             'Crack A': 1,
             'Crack B': 2,
             'Crack C': 3,
-            'Finger Failure': 4,
-            'No Failure': 0
+            'Finger Failure': 4
         }
 
         lab = []
@@ -148,21 +197,10 @@ class solar_panel_data():
         return lab
 
 
-
-# def get_bounding_box(x) :
-#     result = [[100.0,100.0,200.0,200.0]] # dummy values obviously
-#     print("get_bounding_box(x)", x, "result", result)
-#     return result
-
-# def get_label(x):
-#     result = ["bla"] # dummy values obviously
-#     print("get_label(x)", x, "result", result)
-#     return result
-
-
 def DisplayTargetMask(target, idx):
     mask = target["masks"][idx].numpy()
     mask = mask * 255
+
     # Show image
     cv2.imshow('Image', mask)
     # Show image
@@ -185,44 +223,47 @@ def DisplayAllFaults(masks):
             print(Labelstemp)
             print(i)
 
+
+
 def main():
     # Path to directories
-    ImageDir = "../data/Serie1_CellsAndGT/CellsCorr/"
-    GTDir = "../data/Serie1_CellsAndGT/MaskGT/"
+    ImageDir = "../../data/Serie1_CellsAndGT/CellsCorr/"
+    GTDir = "../../data/Serie1_CellsAndGT/MaskGT/"
+    # GTDir = "../../data/Serie1_CellsAndGT/mask/"
 
-    dl = solar_panel_data(ImageDir, GTDir)
+    dl = solar_panel_data(ImageDir, GTDir, removeNoLabels = True)
 
-    num = 4494#8697 #4494
+
+
+
+    # dl.PrintPaths()
+
+    # num = 8499#8500#8512#8511#8697 #4494
+    num = 91
     im, target = dl.__getitem__(num)
 
+
     print(target["label_str"])
-    print(target["labels"])
+    print(target["labels"].numpy())
 
     boxes = target["boxes"].numpy().astype(np.uint32)
-    for i in range(len(boxes)):
-        cv2.rectangle(im,(boxes[i][0],boxes[i][1]),(boxes[i][2],boxes[i][3]),(0,255,0),2)
-        if i == 0:
-            cv2.putText(im,target["label_str"][i], (100,100),1,0.8,(0,255,0),1)
-        else:
-            cv2.putText(im,target["label_str"][i], (boxes[i][2]-60,boxes[i][3]-20),1,0.8,(0,255,0),1)
-    
+    print(boxes)
+
+    print(f'Area: {target["area"]}')
+
+
+    if (len(boxes[0] > 0)):
+        for i in range(len(boxes)):
+            cv2.rectangle(im,(boxes[i][0],boxes[i][1]),(boxes[i][2],boxes[i][3]),(0,255,0),2)
+            cv2.putText(im,target["label_str"][i], (boxes[i][2]-60,boxes[i][3]+20),1,0.8,(0,255,0),1)    
+            
+            DisplayTargetMask(target,i)
 
     # DisplayAllFaults(dl.masks)
 
-    DisplayTargetMask(target,1)
     DisplayImg(im)
 
     
-    # print(Labelstemp)
-    # print(np.max(Classification))
-
-    # Classification = Classification * (255 / np.max(Classification))
-    # Classification = Classification.astype(np.uint8)
-
-    # # Show image
-    # cv2.imshow('Image', Classification)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
