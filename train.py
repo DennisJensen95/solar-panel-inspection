6fb286af9551a7ec070ee9406fa27970e68a976e
Dennis Jensen <ngh1adj95@hotmail.com>
2021-01-06 16:45:49 +0100

    Resizing images to (3, 224, 224) format and also bounding box.
    Changed the images to be loaded as RGB because of expected input from
    feature extractor layers. In addition the images and bounding boxes was
    resized.

--- train.py
+++ train.py
@@ -1,15 +1,6 @@
 import os
-from torchvision import models
-from  import data_load
-import torch
-
-
-model = model.vgg16(pretrained=True)
-
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn
-)
+from components.data_loader.data_load import solar_panel_data
 
 os.mkdir("Results-Folder")
 with open("./Results-Folder/result.txt", "wb") as file:
-    file.write("Stefan er nice og det samme er Martin")
+    file.write(b"Stefan er nice og det samme er Martin")
