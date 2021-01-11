d3a0fee4359fed588857f22d17d003249a5fc0b5
Dennis Jensen <ngh1adj95@hotmail.com>
2021-01-11 10:06:59 +0100

    fixed load

--- DevOps/HPC_trainer/conf/QueJob.sh
+++ DevOps/HPC_trainer/conf/QueJob.sh
@@ -9,6 +9,6 @@
 #BSUB -e logs/%J.err 
 module load python3/3.8.2 
 module load cuda/10.1 
-module laod cudnn/v8.0.4.30-prod-cuda-10.1
+module load cudnn/v8.0.4.30-prod-cuda-10.1
 echo "Running script..."
 python3 train.py
