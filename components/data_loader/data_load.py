Losses correct and normalization implemented

components/data_loader/data_load.py
@@ -23,7 +23,8 @@ class solar_panel_data:
         train,
         filter=True,
         area_limit=100,
-        mask="faster"
+        mask="faster",
+        normalize=False
     ):
         if mask == "mask":
             self.mask = True
@@ -36,11 +37,14 @@ class solar_panel_data:
         self.files, self.masks = self.Load()
 
         self.label_dic = LabelEncoder()
-        #self.label_dic = {"Crack A": 1, "Crack B": 2, "Crack C": 3, "Finger Failure": 4}
+        self.normalize = normalize
         
         self.csv_filepath = "./data/available_files.csv"
         
         self.train = train
+        
+        self.norm_mean = (0.485, 0.456, 0.406) 
+        self.norm_std = (0.229, 0.224, 0.225)
         
         if os.path.exists(self.csv_filepath):
             print("Load csv")
@@ -198,36 +202,19 @@ class solar_panel_data:
         # Resize
         # print(f'Before resize: {np.shape(np.array(masks))}')
 
-        trans1 = transforms.ToTensor()
-
         resize = transforms.Resize(size=(224, 224))
+        norm = transforms.Normalize(self.norm_mean, self.norm_std)
+        image = TF.to_tensor(image)
         image = resize(image)
+        if self.normalize:
+            image = norm(image)
         
         new_masks = []
         for i, mask in enumerate(masks):
             new_masks.append(resize(mask))
         masks = new_masks
 
-
-        # # Random horizontal flipping
-        # if random.random() > 0.5 and train:
-        #     image = TF.hflip(image)
-        #     new_masks = []
-        #     for i, mask in enumerate(masks):
-        #         new_masks.append(TF.hflip(mask))
-        #     masks = new_masks
-
-        # # Random vertical flipping
-        # if random.random() > 0.5 and train:
-        #     image = TF.vflip(image)
-        #     new_masks = []
-        #     for i, mask in enumerate(masks):
-        #         new_masks.append(TF.vflip(mask))
-        #     masks = new_masks
-
-
         # Transform to tensor
-        image = TF.to_tensor(image)
         new_masks = []
         for i, mask in enumerate(masks):
             # new_masks.append(TF.to_tensor(mask))
@@ -460,10 +447,21 @@ class solar_panel_data:
         self.files = dataframe["CellsCorr"]
         self.masks = dataframe["MaskGT"]
 
+def inv_normalize(img):
+    norm_mean = (0.485, 0.456, 0.406)
+    norm_std = (0.229, 0.224, 0.225)
+    invTrans = transforms.Compose([ transforms.Normalize(mean = [ 0., 0., 0. ], 
+                                                         std = [ 1/norm_std[0], 1/norm_std[1], 1/norm_std[2] ]),
+                                    transforms.Normalize(mean = [ -norm_mean[0], -norm_mean[1], -norm_mean[2] ], 
+                                                         std = [ 1., 1., 1. ]),
+                                    ])
+    img = invTrans(img)
+    
+    return img
+
 class LabelEncoder:
 
     def __init__(self, binary=False):
-
         if binary:
             self.fault_value_to_key = {'Crack A': 1,
                                        'Crack B': 1,

train.py
@@ -65,6 +65,7 @@ def train_one_epoch(model, optimizer, data_loader, data_loader_test, device, epo
         lr_scheduler = utils.warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor)
     
     i = 0
+    losses_val = 0
     for images, targets in data_loader:
         images = list(image.to(device) for image in images)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
@@ -73,11 +74,13 @@ def train_one_epoch(model, optimizer, data_loader, data_loader_test, device, epo
 
         losses = sum(loss for loss in loss_dict.values())
 
+        losses_val += losses
+        
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
         loss_value = losses_reduced.item()
+        
 
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
@@ -91,12 +94,9 @@ def train_one_epoch(model, optimizer, data_loader, data_loader_test, device, epo
         if lr_scheduler is not None:
             lr_scheduler.step()
         
-        # if i % 60 == 0 and i != 0:
-        #     success_percentage = evaluate(model, data_loader_test, device)
-        #     print(f"Epoch: {epoch}, itr.: {i}: loss {losses} Accuracy: {success_percentage*100}")
-        # i=i+1
+        i += 1
 
-    return losses
+    return losses_val/i
 
 @torch.no_grad()
 def evaluate(model, data_loader_test, device):
@@ -217,7 +217,8 @@ def train():
         mask_dir,
         filter=True,
         mask=configuration["Model"],
-        train=True
+        train=True,
+        normalize=True
     )
 
     dataset_test = copy.deepcopy(dataset_train)
@@ -230,7 +231,7 @@ def train():
 
     data_loader_train = torch.utils.data.DataLoader(
         dataset_train,
-        batch_size=5,
+        batch_size=configuration["BatchSize"],
         shuffle=True,
         num_workers=4,
         collate_fn=utils.collate_fn,
@@ -255,7 +256,7 @@ def train():
             
     
     # Predefined values
-    epochs = 1
+    epochs = 150
     i = 0
 
     # Optimizer
@@ -301,6 +302,8 @@ def train():
         print(f'Actual Yes  |        {Tpos}          |       {Fneg}')
         print(f'Actual No   |        {Fpos}          |       {Tneg}')
 
+        print(f'Epoch: {epoch}, Loss: {losses}')
+        
         accuracy_vec.append(accuracy)
         success_percent_vec.append(success_percent)
         Tpos_vec.append(Tpos)